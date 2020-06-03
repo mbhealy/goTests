65911d2023f05cfdfc0f400e95021824ba5c2887
Michael Healy <mbhealy83@gmail.com>
2020-06-03 14:14:54 -0400

    Comments on variables

--- variables.go
+++ variables.go
@@ -18,4 +18,13 @@ func main() {
 	
 	f := "apple"
 	fmt.Println(f)
+
+	g := 42
+	fmt.Println(g)
+	// var g:= 42 is incorrect
+	// g int := 42 is incorrect
+	// int g := 42 is incorrect
+	// g := int 42 is incorrect
+	// g := 42 int is incorrect
+	// the only type declaration is the value itself
 }
