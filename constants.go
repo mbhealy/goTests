a6154ff11de306bb46a1057f6ca8889fa9d86232
Michael Healy <mbhealy83@gmail.com>
2020-06-03 16:19:07 -0400

    Added constants.go

---
+++ constants.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "fmt"
+    "math"
+)
+
+const s string = "constant"
+
+func main() {
+	fmt.Println(s)
+	
+	const n = 500000000
+	
+	const d = 3e20 / n
+	fmt.Println(d)
+	
+	fmt.Println(int64(d))
+
+	fmt.Println(math.Sin(n))
+
+	c := n*n*d
+	fmt.Println(c)
+
+	c = c*c
+	fmt.Println(c)
+}
